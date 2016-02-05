c52e1024da1572f12722977e5fd7f12d1f918dfe
csc <s_schlesing@stud.hwr-berlin.de>
2016-02-05 13:42:42 +0100

    initial commit

---
+++ Backend/src/main/java/de/hwrberlin/it2014/sweproject/rest/HelloWorldResource.java
@@ -0,0 +1,28 @@
+package de.hwrberlin.it2014.sweproject.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by csc on 13.01.16.
+ * 
+ * Test Resource für REST WS
+ */
+@Path("/helloworld")
+public class HelloWorldResource {
+
+	/**
+	 * Testmethode um den REST WS zu prüfen
+	 * 
+	 * @return Response
+	 */
+    @GET
+    @Path("/test")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response testMethod(){
+        return Response.status(Response.Status.OK).entity("Hallo, Welt!").build();
+    }
+}
