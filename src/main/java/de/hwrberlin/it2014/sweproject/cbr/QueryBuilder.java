9d1515be3de7e3b8d9e077f9376b13632f50b823
Danilo <danilo.guenzel@web.de>
2016-02-11 15:58:14 +0100

    Changed Querybuilder.buildQuery to static -> no Konstruktor requiered anymore

--- src/main/java/de/hwrberlin/it2014/sweproject/cbr/QueryBuilder.java
+++ src/main/java/de/hwrberlin/it2014/sweproject/cbr/QueryBuilder.java
@@ -10,28 +10,18 @@ import java.util.ArrayList;
  */
 public class QueryBuilder {
 	
-	ArrayList<String> keysWithSyn = null;
-	
-	/**
-	 * Konstruktor erstellt eine leere ArrayList, in der nach Methodenaufruf von BuildQuery alle Schlüsselwörter und deren Synonyme stehen
-	 */
-	public QueryBuilder(){
-		this.keysWithSyn = new ArrayList<String>();
-	}
 	/**
 	 * 
 	 * @param keywords Sind die von der Nutzereingabe erhaltenen Schlüsselwörter(ohne synonyme)
 	 * @return Als Rückgabe erhält man die Datenbankanfrage, die die Zeilen nach den Schlüsselworttreffern auswählt
 	 */
-	private String buildQuery(String[] keywords){
+	public static String buildQuery(String[] keywords){
 		String query = "SELECT * FROM tbl_judgement WHERE CONTAINS((sentence, offence, keywords),";
 		String queryKeywords = " '";
 		
 		for(String key : keywords){
-//			this.keysWithSyn.add(key);
 			queryKeywords += key.toLowerCase() + "','";
 			for(String syn : ThesaurusLoader.getSynonyms(key)){
-//				keysWithSyn.add(syn);
 				queryKeywords += syn.toLowerCase() + "','";
 			}
 		}
