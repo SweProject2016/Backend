4965b3c479afa001e2aafb1bf02bfa1eaf4500ef
Max Bock <info@max-bock.de>
2016-02-14 13:51:26 +0100

    added CBR and Case-classes to project

---
+++ src/main/java/de/hwrberlin/it2014/sweproject/cbr/CBR.java
@@ -0,0 +1,46 @@
+package de.hwrberlin.it2014.sweproject.cbr;
+
+//import ArrayList;
+
+/**
+ * controls the cbr-cycle. Start the algorithm with startCBR(ArrayList<String>) method.
+ * @author Max
+ * 
+ */
+public class CBR {
+	
+	private ArrayList<Case> activeCases;
+	public HTTPResponse startCBR(ArrayList<String> usersInput)
+	{
+		//calls retrieve:CaseList
+		//calls reuse and revise
+		//build HTTPResponse
+	}
+	
+	public HTTPResponse saveUserEvaluate(Case evaluatedCase)
+	{
+		//calls retain and write Case to DB
+	}
+	
+	private ArrayList<Case> retrieve(ArrayList<String> usersInput)
+	{
+		//create new CaseObject
+		//new query to DB
+		//recieves a CaseList with similiar cases
+	}
+	
+	private ArrayList<Case> reuse(ArrayList<Case> caseListFromDBQuery)
+	{
+		//edit and control the caselist from DB-query
+	}
+	
+	private ArrayList<Case> revise(ArrayList<Case> caseListFromReuse)
+	{
+		//build HTTPResponse
+	}
+	
+	private void retain(Case evaluatedCase)
+	{
+		
+	}
+}
---
+++ src/main/java/de/hwrberlin/it2014/sweproject/cbr/Case.java
@@ -0,0 +1,17 @@
+package de.hwrberlin.it2014.sweproject.cbr;
+
+/**
+ * 
+ * @author Max
+ *
+ */
+public class Case {
+	
+	private String description;
+	
+	public Case(ArrayList<String> userInput)
+	{
+		description=userInput.toString();
+	}
+	
+}
