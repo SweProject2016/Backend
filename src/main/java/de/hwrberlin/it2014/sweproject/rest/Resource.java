440f4c3507fa5458b88974beec859a7b4459ffcb
csc <s_schlesing@stud.hwr-berlin.de>
2016-02-27 09:12:32 +0100

    fix: better normalization

--- src/main/java/de/hwrberlin/it2014/sweproject/rest/Resource.java
+++ src/main/java/de/hwrberlin/it2014/sweproject/rest/Resource.java
@@ -44,8 +44,11 @@ public abstract class Resource {
 	}
 	
 	private String normalize(String str){
-		if(str.startsWith("\"") && str.endsWith("\"")){
-			str = str.substring(1, str.length()-1);
+		if(str.startsWith("\"")){
+			str = str.substring(1, str.length());
+		}
+		if(str.endsWith("\"")) {
+			str = str.substring(0,str.length()-1);
 		}
 		return str;
 	}
