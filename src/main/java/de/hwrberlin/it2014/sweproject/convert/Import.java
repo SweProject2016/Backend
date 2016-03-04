297571e61f65d1a1193d3c9d89ee850fad7a7825
csc <s_schlesing@stud.hwr-berlin.de>
2016-03-04 12:22:41 +0100

    minor changes for debugging import

--- src/main/java/de/hwrberlin/it2014/sweproject/convert/Import.java
+++ src/main/java/de/hwrberlin/it2014/sweproject/convert/Import.java
@@ -16,47 +16,47 @@ import de.hwrberlin.it2014.sweproject.userinput.UserInput;
 
 public class Import {
 
-	public Import(){
+    public Import(){
 
-	}
+    }
 
-	public void importCases(String filePath) throws IOException{
-		File[] files = new File(filePath).listFiles();
-		DatabaseConnection con = new DatabaseConnection();
-		boolean connected = con.connectToMysql();
-		for(File file : files) {
-			if(file.isFile()) {
-				Path path = Paths.get(file.getAbsolutePath());
-				Fall fall = FallParser.getFromPdf(path);
-				String text = "";
-				text += fall.getGruende() + " ";
-				text += fall.getVergehen() + " ";
-				text += fall.getRechtsBereich();
-				String keywords = UserInput.getLawTermsFromInput(text);
+    public void importCases(final String filePath) throws IOException{
+        File[] files = new File(filePath).listFiles();
+        DatabaseConnection con = new DatabaseConnection();
+        boolean connected = con.connectToMysql();
+        for(File file : files) {
+            if(file.isFile()) {
+                Path path = Paths.get(file.getAbsolutePath());
+                Fall fall = FallParser.getFromPdf(path);
+                String text = "";
+                text += fall.getGruende() + " ";
+                text += fall.getVergehen() + " ";
+                text += fall.getRechtsBereich();
+                String keywords = UserInput.getLawTermsFromInput(text);
 
-				System.out.println(connected);
-				Date da = new Date();
-				java.sql.Date d = new java.sql.Date(da.getTime());
+                System.out.println(connected);
+                Date da = new Date();
+                java.sql.Date d = new java.sql.Date(da.getTime());
 
-				Judgement j = new Judgement(file.getAbsolutePath(), file.getName());
-				j.setDate(d);
-				j.setKeywords(keywords);
-				j.setOffence(fall.getVergehen());
-				j.setPageRank(1.231f);
-				j.setSector(new LawSector(fall.getRechtsBereich()));
-				j.setSentence(fall.getStrafmass());
-				j.setComittee(new Committee("com2"));
-				j.setFileReference(file.getName());
+                Judgement j = new Judgement(file.getAbsolutePath(), file.getName());
+                j.setDate(d);
+                j.setKeywords(keywords);
+                j.setOffence(fall.getVergehen());
+                j.setPageRank(1.231f);
+                j.setSector(new LawSector(fall.getRechtsBereich()));
+                j.setSentence(fall.getStrafmass());
+                j.setComittee(new Committee("com2"));
+                j.setFileReference(file.getName());
 
-				try {
-					con.executeUpdate(TableJudgementSQL.getInsertSQLCode(j));
-				} catch(SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+                try {
+                    con.executeUpdate(TableJudgementSQL.getInsertSQLCode(j));
+                } catch(SQLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
 
-			}
-		}
-		con.close();
-	}
+            }
+        }
+        con.close();
+    }
 }
--- src/main/java/de/hwrberlin/it2014/sweproject/rest/StatusResource.java
+++ src/main/java/de/hwrberlin/it2014/sweproject/rest/StatusResource.java
@@ -4,27 +4,27 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import de.hwrberlin.it2014.sweproject.model.Status.ServerStatus;
 
 @Path("/")
 public class StatusResource extends Resource{
-	
-	@GET
-	@Path("status")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getServerStatus(){
-		try{
-			final String path="/status";
-			de.hwrberlin.it2014.sweproject.model.Status status = new de.hwrberlin.it2014.sweproject.model.Status();
-			status.setDatabaseStatus(ServerStatus.RUNNING);
-			status.setServer(ServerStatus.RUNNING);
-			return build(Status.OK,status,path);
-		} catch(Exception e){
-			e.printStackTrace();
-			return build(Status.INTERNAL_SERVER_ERROR);
-		}
-	}
+
+    @GET
+    @Path("status")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getServerStatus(){
+        try{
+            final String path="/status";
+            de.hwrberlin.it2014.sweproject.model.Status status = new de.hwrberlin.it2014.sweproject.model.Status();
+            status.setDatabaseStatus(ServerStatus.RUNNING);
+            status.setServer(ServerStatus.RUNNING);
+            return build(Status.OK,status,path);
+        } catch(Exception e){
+            e.printStackTrace();
+            return build(Status.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
