4d60022e48f2c9aa89c1a2b945501ab02e9deeae
csc <s_schlesing@stud.hwr-berlin.de>
2016-03-14 11:12:06 +0100

    changed thesaurus to local database

--- src/main/java/de/hwrberlin/it2014/sweproject/synonym/ThesaurusLoader.java
+++ src/main/java/de/hwrberlin/it2014/sweproject/synonym/ThesaurusLoader.java
@@ -1,19 +1,9 @@
 package de.hwrberlin.it2014.sweproject.synonym;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
 
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import de.hwrberlin.it2014.sweproject.database.DatabaseConnection;
+import de.hwrberlin.it2014.sweproject.database.ThesaurusDatabase;
 
 /**
  * Diese Klasse stellt die Verbindung zu der (lokalen)Thesaurus-Datenbank dar, um eingegebene Schl�sselw�rter auch durch Synonyme finden zu k�nnen.
@@ -28,46 +18,9 @@ public class ThesaurusLoader {
 	 * @return Der R�ckgabewert ist ein ArrayList von Strings, die die gefundenen Synonyme enth�lt
 	 */
 	public static ArrayList<String> getSynonyms(final String keyword){
-        StringBuffer response = new StringBuffer();
-        Set<String> syns = new HashSet<>();
+        DatabaseConnection con = new DatabaseConnection();
         ArrayList<String> synonyms = new ArrayList<String>();
-        try {
-            String url = "https://www.openthesaurus.de/synonyme/search?q="+ keyword +"&format=application/json";
- 
-            URL obj = new URL(url);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
- 
-            con.setRequestMethod("GET");
- 
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
- 
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-            
-            
-        // parsing response
-        JSONParser parser = new JSONParser();
-        JSONObject json = (JSONObject) parser.parse(response.toString());
-        JSONArray synsets = (JSONArray) json.get("synsets");
-        for(int i=0;i<synsets.size();i++){
-        	JSONObject aSynset = (JSONObject) synsets.get(i);
-        	JSONArray terms = (JSONArray) aSynset.get("terms");
-        	for(int j=0;j<terms.size();j++){
-        		JSONObject aTerm = (JSONObject) terms.get(j);
-        		String term = (String) aTerm.get("term");
-        		syns.add(term);
-        	}
-        }
-        
-        } catch (ParseException | IOException e) {
-            e.printStackTrace();
-		}
-        //using Set for unique entries
-        synonyms.addAll(syns);
-
+        synonyms.addAll(ThesaurusDatabase.getSynonymsForTerm(keyword, con));
         return synonyms;
 }
 	
